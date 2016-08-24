8abc667d5cb009577f04495826f02a73c20f54cb
leeSu-jeong <SJ@leesu-jeongui-macbook.local>
2016-08-24 09:23:38 +0900

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by leeSu-jeong on 2016. 8. 24..
+//  Copyright © 2016년 crystal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by leeSu-jeong on 2016. 8. 24..
+//  Copyright © 2016년 crystal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
